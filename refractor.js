634dbd2d7eaf642e6b867a8f21eb95aad1af15e5
Bruemor <arymer1@gmail.com>
2016-02-17 14:28:56 -0500

    question 5

--- refractor.js
+++ refractor.js
@@ -143,8 +143,11 @@ var spanishColor = function(colorName) {
 };
 
 // Put your answer below -------------------------
-
-
+var spanishColor = function(colorName) {
+  var colorName = {rojo: '#ff0000', blanco: '#ffffff', azul: '#0000ff', verde: '#00ff00', negro: '#000000'};
+  return colorName.rojo;
+};
+console.log(spanishColor());
 // -----------------------------------------------
 
 //////////////////////////////////////////////////
